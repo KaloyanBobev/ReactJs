953ed3b05be331386e47a26ce2beada26260c10f
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-26 09:51:59 +0100

    create Perimeter component

---
+++ math-formulas-react/src/components/Perimeter/Perimeter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class Perimeter extends React.Component {
+
+    render() {
+        return (
+            <h1>Perimeter</h1>
+        )
+    }
+}
+
+export default Perimeter;
