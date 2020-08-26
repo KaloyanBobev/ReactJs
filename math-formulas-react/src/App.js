83f36ae88a425c159fc2a10591629acd9a115f9f
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-26 09:51:16 +0100

    import pages

--- math-formulas-react/src/App.js
+++ math-formulas-react/src/App.js
@@ -1,11 +1,50 @@
 import React from 'react';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
+import Theory from './components/theory/Theory';
+import Perimeter from './components/Perimeter/Perimeter';
+import Area from './components/Area/Area';
+import Quiz from './components/Quiz/Quiz';
+import NotFound from './components/NotFound/NotFound';
 class App extends React.Component {
 
     render() {
         return (
             <div>
-                <h1>APP</h1>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Theory</a></li>
+                            <li><a href="/perimeter">Perimeter</a></li>
+                            <li><a href="/area">Area</a></li>
+                            <li><a href="/quiz">Quiz</a></li>
+                        </ul>
+                        <BrowserRouter>
+                            <Switch>
+                                <Route
+                                    exact
+                                    path="/"
+                                    component={Theory} />
+
+                                <Route
+                                    exact
+                                    path="/perimeter"
+                                    component={Perimeter} />
+                                <Route
+                                    exact
+                                    path="/area"
+                                    component={Area} />
+                                <Route
+                                    exact
+                                    path="/quiz"
+                                    component={Quiz} />
+                                <Route
+                                    path="/*"
+                                    component={NotFound} />
+                            </Switch>
+                        </BrowserRouter>
+                    </nav>
+                </header>
             </div>
         )
     }
