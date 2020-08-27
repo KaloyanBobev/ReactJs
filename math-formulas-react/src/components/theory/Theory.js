cdac85015c76de119354f8d54915d4af398a2210
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-27 17:58:20 +0100

    add map for infos

--- math-formulas-react/src/components/theory/Theory.js
+++ math-formulas-react/src/components/theory/Theory.js
@@ -6,106 +6,116 @@ class Theory extends React.Component {
         super();
         this.state = {
             infos: [{
+                id: 1,
                 name: 'Square',
                 header: `The Perimeter of Square`,
                 information: `A Square is a regular rectangle but have four equal sides A with 90 degrees angles. `,
                 formula: `P = 4 * A`,
                 explanation: `A is the length of one of the sides.`,
-                src: `perimeter-square.png`,
+                src: `perimeter-square`,
                 variable: 'Side A: '
-
-
             }, {
+                id: 2,
                 name: 'Circle',
-                Headers: `The perimeter of Circle`,
+                header: `The perimeter of Circle`,
                 information: `Perimeter is a length around the circle. `,
                 formula: `P = 2 * π * R`,
                 explanation: `π is constant  equal on 3,1415926535, R is a radius of a circle .`,
-                src: `perimeter-circle.png`,
+                src: `perimeter-circle`,
                 variable: 'Radius R: '
             }, {
+                id: 3,
                 name: 'Triangle',
-                Headers: `The Perimeter of Triangle`,
+                header: `The Perimeter of Triangle`,
                 information: `Perimeter is the sum of three sides of triagle.`,
                 formula: `P = A + B + C`,
                 explanation: `A, B and C are the length of each of the sides.`,
-                src: `perimeter-triangle.png`,
+                src: `perimeter-triangle`,
                 variable: ['Side A: ', 'Side B: ', 'Side C: ']
             }, {
+                id: 4,
                 name: 'Rectangle',
                 header: `The Perimeter of Rectangle`,
                 information: `Perimeter is the sum of two times length and two times width. `,
                 formula: `P = 2 * A + 2 * B `,
                 explanation: `A is a length and B is a width.`,
-                src: `perimeter-rectangle.png`,
+                src: `perimeter-rectangle`,
                 variable: ['Side A: ', 'Side B: ']
             }, {
+                id: 5,
                 name: 'Polygon',
                 header: `The Perimeter of regular Polygon`,
                 information: `Perimeter is the sum all equal sides of polygon.`,
                 formula: `P = X * A `,
                 explanation: `A is a length of one of the sides and X is number of the sides.`,
-                src: `perimeter-polygon.png`,
+                src: `perimeter-polygon`,
                 variable: ['Side A: ', 'Number X: ']
-            },
-            {
+            }, {
+                id: 6,
                 name: 'Square',
                 header: `The Area of Square`,
                 information: `A Square is a regular rectangle but have four equal sides A with 90 degrees angles. `,
                 formula: `Area =  A * A`,
                 explanation: `A is the length of one of the sides.`,
-                src: `area-square.png`,
+                src: `area-square`,
                 variable: 'Side A: '
 
             }, {
+                id: 7,
                 name: 'Circle',
                 header: `The Area of Circle`,
                 information: `The area of a circle is encolsed from his perimeter. `,
                 formula: `Area = π * R * R`,
                 explanation: `π is constant equal on 3,1415926535, R is a radius of a circle .`,
-                src: `perimeter-circle.png`,
+                src: `perimeter-circle`,
                 variable: 'Riadius R: '
 
             }, {
+                id: 8,
                 name: 'Triangle',
                 header: `The Area of Triangle`,
                 information: `This is a polygon with three vertices and  three edges.`,
                 formula: `Area = 1/2 * B * H`,
                 explanation: `B is a length of the base and H is vertical height.`,
-                src: `area-triangle.png`,
+                src: `area-triangle`,
                 variable: ['Side B: ', 'Height H: ']
             }, {
+                id: 9,
                 name: 'Rectangle',
                 header: `The Area of Rectangle`,
                 information: ` Rectange is a quadrilateral with four right angles `,
                 formula: `Area = W * H `,
                 explanation: `W is a width and H is a height.`,
-                src: `area-rectangle.png`,
+                src: `area-rectangle`,
                 variable: ['Width W: ', 'Height H: ']
             }, {
+                id: 10,
                 name: 'Polygon',
                 header: `The Area of regular Polygon`,
                 information: `A regular polygon is a polygon where each of the sides have the same length and also equal angles`,
                 formula: `Area = 1/2 * P * A `,
                 explanation: `P is a perimeter and A is for apothem.`,
-                src: `area-polygon.png`,
+                src: `area-polygon`,
                 variable: ['Perimeter P: ', 'Apothem A: ']
             }]
         }
     }
 
-
     render() {
         return (
             <section>
-                <article>
-                    <h1>Theory for square, circle, triangle, rectangle and polygon</h1>
-                    <h3>{this.state.infos[0].header}</h3>
-                    <img src={require('../../assests/perimeter-square.png')} alt="problem" />
-                    <div>Information: {this.state.infos[0].information}</div>
-                    <div> Formula: {this.state.infos[0].formula}</div>
-                    <div>Explanation: {this.state.infos[0].explanation}</div>
-                </article>
+                <h1>Theory for square, circle, triangle, rectangle and polygon</h1>
+                {this.state.infos.map((value, index) => {
+                    return (
+                        <article>
+                            <h3 key={index}>{value.header}</h3>
+                            <img key={index} src={require("../../assests/" + value.src + ".png")} alt="problem" />
+                            <div key={index}>Information: {value.information}</div>
+                            <div key={index}> Formula: {value.formula}</div>
+                            <div key={index}>Explanation: {value.explanation}</div>
+                        </article>
+                    )
+                })}
             </section>
         )
     }
