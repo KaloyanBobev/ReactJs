18a7b86ace81470c63ea3dbf7f8ade38aeff2478
kaloyanbobev <kaloyanbobev@gmail.com>
2020-08-28 10:40:07 +0100

    update

--- math-formulas-react/src/components/Area/Area.js
+++ math-formulas-react/src/components/Area/Area.js
@@ -4,7 +4,10 @@ class Area extends React.Component {
 
     render() {
         return (
-            <h1>AREA</h1>
+            <section>
+                <h1>AREA</h1>
+            </section>
+
         )
     }
 }
