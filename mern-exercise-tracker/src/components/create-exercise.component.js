cabfd3d7196a13ca1b7df62994b7a9b50f805b13
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-02 15:34:06 +0100

    update

--- mern-exercise-tracker/src/components/create-exercise.component.js
+++ mern-exercise-tracker/src/components/create-exercise.component.js
@@ -7,11 +7,6 @@ export default class CreateExercises extends React.Component {
     constructor(props) {
         super(props);
 
-        this.onChangeUsername = this.onChangeUsername.bind(this);
-        this.onChangeDescription = this.onChangeDescription.bind(this);
-        this.onChangeDuration = this.onChangeDuration.bind(this);
-        this.onChangeDate = this.onChangeDate.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
         this.state = {
             username: '',
             description: '',
@@ -20,6 +15,11 @@ export default class CreateExercises extends React.Component {
             users: [],
         }
 
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
     componentDidMount() {
