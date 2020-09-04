8876af0922de471e9d2e7294e65529ad703ed02a
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-04 15:38:53 +0100

    add data

--- math-formulas-react/src/components/Quiz/Quiz.js
+++ math-formulas-react/src/components/Quiz/Quiz.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import quiz from '../../data/quiz.json';
+
 
 class Quiz extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            answerOne: "",
+            quiz: []
+        }
+        this.OnChange = this.OnChange.bind(this);
+    }
 
+    componentDidMount() {
+        this.setState({
+            quiz: quiz
+        })
+    }
+
+    OnChange(e) {
+        this.setState({
+            answerOne: e.target.value
+        })
+        console.log(this.answerOne);
+    }
     render() {
         return (
             <section>
-                <h1>QUIZ</h1>
+                <h1>Maths Quiz</h1>
+                {this.state.quiz.map((value) => {
+                    return (
+                        <article key={value.id}>
+                            <h3 >Question {value.id + 1}</h3>
+                            <div >Task: {value.task}</div>
+                            <img src={require("../../assests/" + value.src + ".png")} alt="problem" />
+                            <label>Write your answer here:</label>
+                            <input
+                                type="text"
+                                value={this.state.answerOne}
+                                onClick={this.OnChange} />
+
+                        </article>
+                    )
+                })}
             </section>
 
         )
