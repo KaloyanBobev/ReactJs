cabcf29e8e1ae391c0be9a5ee64252bf866392ad
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-04 15:38:40 +0100

    add data

--- math-formulas-react/src/components/Area/Area.js
+++ math-formulas-react/src/components/Area/Area.js
@@ -1,13 +1,34 @@
 import React from 'react';
-
+import data from '../../data/data.json'
 class Area extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            infos: []
+        }
+    }
 
+    componentDidMount() {
+        this.setState({
+            infos: data
+        })
+    }
     render() {
         return (
             <section>
-                <h1>AREA</h1>
+                <h1>Area</h1>
+                {this.state.infos.slice(5, 10).map((value) => {
+                    return (
+                        <article key={value.id}>
+                            <h3 >{value.header}</h3>
+                            <img src={require("../../assests/" + value.src + ".png")} alt="problem" />
+                            <div >Information: {value.information}</div>
+                            <div > Formula: {value.formula}</div>
+                            <div >Explanation: {value.explanation}</div>
+                        </article>
+                    )
+                })}
             </section>
-
         )
     }
 }
