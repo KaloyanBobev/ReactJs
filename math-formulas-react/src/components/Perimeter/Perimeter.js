4c46cac87ecbf04e385980fb37cd5022ba24e894
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-04 15:38:46 +0100

    add data

--- math-formulas-react/src/components/Perimeter/Perimeter.js
+++ math-formulas-react/src/components/Perimeter/Perimeter.js
@@ -1,12 +1,33 @@
 import React from 'react';
-
+import data from '../../data/data.json'
 
 class Perimeter extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            infos: []
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ infos: data })
+    }
 
     render() {
         return (
             <section>
                 <h1>Perimeter</h1>
+                {this.state.infos.slice(0, 5).map((value) => {
+                    return (
+                        <article key={value.id}>
+                            <h3 >{value.header}</h3>
+                            <img src={require("../../assests/" + value.src + ".png")} alt="problem" />
+                            <div >Information: {value.information}</div>
+                            <div > Formula: {value.formula}</div>
+                            <div >Explanation: {value.explanation}</div>
+                        </article>
+                    )
+                })}
             </section>
         )
     }
