bdb3ef6003c29bb585a246976bb6f491bd476536
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-07 10:19:53 +0100

    update with Section component

--- math-formulas-react/src/components/theory/Theory.js
+++ math-formulas-react/src/components/theory/Theory.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import data from '../../data/data.json';
-
+import Section from './Section';
 class Theory extends React.Component {
     constructor() {
         super();
@@ -15,21 +15,13 @@ class Theory extends React.Component {
     }
 
     render() {
+        const title = "Theory for square, circle, triangle, rectangle and polygon";
         return (
-            <section>
-                <h1>Theory for square, circle, triangle, rectangle and polygon</h1>
-                {this.state.infos.map((value) => {
-                    return (
-                        <article key={value.id}>
-                            <h3 >{value.header}</h3>
-                            <img src={require("../../assests/" + value.src + ".png")} alt="problem" />
-                            <div >Information: {value.information}</div>
-                            <div > Formula: {value.formula}</div>
-                            <div >Explanation: {value.explanation}</div>
-                        </article>
-                    )
-                })}
-            </section>
+            <Section
+                title={title}
+                info={this.state.infos}
+                isShow={false}
+            />
         )
     }
 }
