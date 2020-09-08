1c60af830e897d5ae19e5e9f48534999808d1801
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-08 18:31:07 +0100

    add Quiestion component and CheckResult function

--- math-formulas-react/src/components/Quiz/Quiz.js
+++ math-formulas-react/src/components/Quiz/Quiz.js
@@ -1,48 +1,101 @@
 import React from 'react';
 import quiz from '../../data/quiz.json';
-
+import Question from './Question';
 
 class Quiz extends React.Component {
     constructor() {
         super();
         this.state = {
-            answerOne: "",
-            quiz: []
-        }
-        this.OnChange = this.OnChange.bind(this);
-    }
-
-    componentDidMount() {
-        this.setState({
+            answer1: "",
+            answer2: "",
+            answer3: "",
+            answer4: "",
+            answer5: "",
             quiz: quiz
-        })
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.CheckResult = this.CheckResult.bind(this);
     }
 
-    OnChange(e) {
-        this.setState({
-            answerOne: e.target.value
-        })
-        console.log(this.answerOne);
+    CheckResult(num, a) {
+        if (this.state.quiz[num].answer === a) {
+            return <span className="right"> This is the right answer</span>
+        } else {
+            return <span className="wrong"> Wrong answer</span>
+        }
+    }
+
+    handleChange(event) {
+        const { name, value } = event.target;
+        this.setState({ [name]: value })
+
     }
     render() {
+        const title = "Maths Quiz";
+
         return (
             <section>
-                <h1>Maths Quiz</h1>
-                {this.state.quiz.map((value) => {
-                    return (
-                        <article key={value.id}>
-                            <h3 >Question {value.id + 1}</h3>
-                            <div >Task: {value.task}</div>
-                            <img src={require("../../assests/" + value.src + ".png")} alt="problem" />
-                            <label>Write your answer here:</label>
-                            <input
-                                type="text"
-                                value={this.state.answerOne}
-                                onClick={this.OnChange} />
-
-                        </article>
-                    )
-                })}
+                <h1>{title}</h1>
+                <article>
+                    <Question
+                        number={0}
+                        infos={this.state.quiz} />
+                    <label>Write your answer here:</label>
+                    <input
+                        type="text"
+                        name={"answer1"}
+                        value={this.state.answer1}
+                        onChange={this.handleChange} />
+                    {this.CheckResult(0, this.state.answer1)}
+                </article>
+                <article>
+                    <Question
+                        number={1}
+                        infos={this.state.quiz} />
+                    <label>Write your answer here:</label>
+                    <input
+                        type="text"
+                        name={"answer2"}
+                        value={this.state.answer2}
+                        onChange={this.handleChange} />
+                    {this.CheckResult(1, this.state.answer2)}
+                </article>
+                <article>
+                    <Question
+                        number={2}
+                        infos={this.state.quiz} />
+                    <label>Write your answer here:</label>
+                    <input
+                        type="text"
+                        name={"answer3"}
+                        value={this.state.answer3}
+                        onChange={this.handleChange} />
+                    {this.CheckResult(2, this.state.answer3)}
+                </article>
+                <article>
+                    <Question
+                        number={3}
+                        infos={this.state.quiz} />
+                    <label>Write your answer here:</label>
+                    <input
+                        type="text"
+                        name={"answer4"}
+                        value={this.state.answer4}
+                        onChange={this.handleChange} />
+                    {this.CheckResult(3, this.state.answer4)}
+                </article>
+                <article>
+                    <Question
+                        number={4}
+                        infos={this.state.quiz} />
+                    <label>Write your answer here:</label>
+                    <input
+                        type="text"
+                        name={"answer5"}
+                        value={this.state.answer5}
+                        onChange={this.handleChange} />
+                    {this.CheckResult(4, this.state.answer5)}
+                </article>
             </section>
 
         )
