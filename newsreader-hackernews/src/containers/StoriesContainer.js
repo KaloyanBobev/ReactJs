c2358fb4a13bfd0c659478b93add5e55031b64e8
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-14 10:43:02 +0100

    create new stories container

---
+++ newsreader-hackernews/src/containers/StoriesContainer.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import { getStoryIds, getStory } from '../services/hnApi';
+
+export const StoriesContainer = () => {
+    const [storyIds, setStoryIds] = useState([]);
+
+    useEffect(() => {
+        getStoryIds().then(data => setStoryIds(data));
+        getStory(24468338).then(data => console.log(data));
+    }, []);
+
+    return (
+        <p>{JSON.stringify(storyIds)}</p>
+    )
+}
