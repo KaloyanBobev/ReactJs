cb22581393cd36b7bba4b42097447446cbf62ae8
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-14 18:47:08 +0100

    update

--- newsreader-hackernews/src/containers/StoriesContainer.js
+++ newsreader-hackernews/src/containers/StoriesContainer.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { getStoryIds, getStory } from '../services/hnApi';
-
+import { getStoryIds } from '../services/hnApi';
+import { Story } from '../components/Story';
 export const StoriesContainer = () => {
     const [storyIds, setStoryIds] = useState([]);
 
     useEffect(() => {
         getStoryIds().then(data => setStoryIds(data));
-        getStory(24468338).then(data => console.log(data));
+        // getStory(24468338).then(data => console.log(data));
     }, []);
 
     return (
-        <p>{JSON.stringify(storyIds)}</p>
+        <div>
+            <h1>Hacker News Stories</h1>
+            {storyIds.map(storyId => <Story key={storyId} storyId={storyId} />
+            )}
+        </div>
+
     )
 }
