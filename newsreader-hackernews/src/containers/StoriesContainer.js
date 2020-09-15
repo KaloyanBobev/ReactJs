b7a22d1432efec60e278ca64d667f8f3a5a886c0
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-15 17:25:29 +0100

    Update StoriesContainer.js

--- newsreader-hackernews/src/containers/StoriesContainer.js
+++ newsreader-hackernews/src/containers/StoriesContainer.js
@@ -2,9 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { getStoryIds } from '../services/hnApi';
 import { Story } from '../components/Story';
 import { GlobalStyle, StoriesConteinerWrapper } from '../styles/StoryContainerStyles';
+import { useInfinetiteScroll } from '../hooks/useInfiniteScroll';
+
 
 export const StoriesContainer = () => {
+    const { count } = useInfinetiteScroll();
     const [storyIds, setStoryIds] = useState([]);
+
+
 
     useEffect(() => {
         getStoryIds().then(data => setStoryIds(data));
@@ -16,7 +21,7 @@ export const StoriesContainer = () => {
             <GlobalStyle />
             <StoriesConteinerWrapper data-test-id="stories-container">
                 <h1>Hacker News Stories</h1>
-                {storyIds.map(storyId => (<Story key={storyId}
+                {storyIds.slice(0, count).map(storyId => (<Story key={storyId}
                     storyId={storyId} />
                 ))}
             </StoriesConteinerWrapper>
