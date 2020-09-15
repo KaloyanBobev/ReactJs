825604585a1c505a8b5d94e4f512252c4ba84026
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-15 09:13:17 +0100

    add styles

--- newsreader-hackernews/src/containers/StoriesContainer.js
+++ newsreader-hackernews/src/containers/StoriesContainer.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { getStoryIds } from '../services/hnApi';
 import { Story } from '../components/Story';
+import { GlobalStyle, StoriesConteinerWrapper } from '../styles/StoryContainerStyles';
+
 export const StoriesContainer = () => {
     const [storyIds, setStoryIds] = useState([]);
 
@@ -11,9 +13,13 @@ export const StoriesContainer = () => {
 
     return (
         <div>
-            <h1>Hacker News Stories</h1>
-            {storyIds.map(storyId => <Story key={storyId} storyId={storyId} />
-            )}
+            <GlobalStyle />
+            <StoriesConteinerWrapper data-test-id="stories-container">
+                <h1>Hacker News Stories</h1>
+                {storyIds.map(storyId => (<Story key={storyId}
+                    storyId={storyId} />
+                ))}
+            </StoriesConteinerWrapper>
         </div>
 
     )
