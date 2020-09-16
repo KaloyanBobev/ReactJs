0820ebd7498922b3746f065374da6b9e2d379102
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-16 18:35:33 +0100

    create books and newspapers components

---
+++ literaturen-glas/src/components/Main/Newspaers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Newspapers extends React.Component {
+
+
+    render() {
+        return (
+            <h1>NEWSPEPERS</h1>
+        )
+    }
+}
+export default Newspapers;
