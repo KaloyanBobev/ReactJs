8c5c7bc3955a335338fe1150f80fc65b0f1e2934
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-16 18:35:53 +0100

    create photogallery

---
+++ literaturen-glas/src/components/PhotoGallery/PhotoGallery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PhotoGallery extends React.Component {
+    render() {
+        return (
+            <h1>PhotoGallery</h1>
+        )
+    }
+}
+
+export default PhotoGallery;
