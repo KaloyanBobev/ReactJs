e903949d066a05131bca0da4bbd1496425ac8da6
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-21 11:51:03 +0100

    create Newspapers JSX

--- literaturen-glas/src/components/Main/Newspaers.js
+++ literaturen-glas/src/components/Main/Newspaers.js
@@ -1,11 +1,45 @@
 import React from 'react';
 
-class Newspapers extends React.Component {
+import newspapers from '../../data/newspaper.json';
 
+class Newspapers extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            news: []
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            news: newspapers
+        })
+    }
 
     render() {
         return (
-            <h1>NEWSPEPERS</h1>
+            <section>
+                {this.state.news.reverse().map((value) => {
+                    return (
+                        <article key={value.href}>
+                            <h2>{value.title}</h2>
+                            <h3>{value.date}</h3>
+                            <button>
+                                <a href={require(`../../newspapers/${value.href}`)} rel="noopener noreferrer" target="_blank">
+                                    <span class="glyphicon glyphicon-file"></span>Прочети
+                                    </a>
+                            </button>
+
+
+                            <button>
+                                <a href={require(`../../newspapers/${value.href}`)} download>
+                                    <span class="glyphicon glyphicon-download-alt"></span> Свали
+                            </a>
+                            </button>
+                        </article>
+                    )
+                })}
+            </section>
         )
     }
 }
