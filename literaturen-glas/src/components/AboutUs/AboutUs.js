36118bdf2da4bce8c3cb99f4cdcefa4701fc463c
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-22 18:14:16 +0100

    create the about us page

--- literaturen-glas/src/components/AboutUs/AboutUs.js
+++ literaturen-glas/src/components/AboutUs/AboutUs.js
@@ -1,9 +1,36 @@
 import React from 'react';
-
+import center from '../../data/home.json';
 class AboutUs extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            isTextVisible: false,
+            info: []
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+    componentDidMount() {
+        this.setState({ info: center })
+    }
+
+    handleChange() {
+        this.setState(prevState => ({ isTextVisible: !prevState.isTextVisible }))
+    }
+
     render() {
         return (
-            <h1>AboutUs</h1>
+            <div id="second-article" className="col-md-8 offset-md-2">
+                <h2>{this.state.info.centerHeading}</h2>
+                <div>{this.state.info.fundationCenter}</div>
+                <div>{this.state.info.history}</div>
+                {this.state.isTextVisible
+                    ? <div>
+                        <div>{this.state.info.art}</div>
+                        <div>{this.state.info.base}</div></div>
+                    : null}
+                <a href="#second-article" onClick={this.handleChange}>
+                    {this.state.isTextTwoVisible ? "Прочети  по-малко" : "Прочети повече"}</a>
+            </div>
         )
     }
 }
