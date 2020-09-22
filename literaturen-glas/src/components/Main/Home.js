d642767731d723676f52b6b8ea293ded69cc3842
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-22 18:15:07 +0100

    create home page

--- literaturen-glas/src/components/Main/Home.js
+++ literaturen-glas/src/components/Main/Home.js
@@ -1,34 +1,45 @@
 import React from 'react';
 import center from '../../data/home.json'
-
+import './Home.scss';
 class Home extends React.Component {
     constructor() {
         super();
         this.state = {
-            info: ""
+            isTextVisible: false,
+            info: []
         }
+        this.handleChange = this.handleChange.bind(this);
+
     }
 
     componentDidMount() {
         this.setState({ info: center })
-
     }
+
+    handleChange() {
+        this.setState(prevState => ({ isTextVisible: !prevState.isTextVisible }))
+    }
+
+
     render() {
         return (
-            <article>
-                <h1>{this.state.info.mainHeader}</h1>
-                <h2>{this.state.info.teacher}</h2>
-                <h2>{this.state.info.voice}</h2>
-                <h2>{this.state.info.chairman}</h2>
-                <div>{this.state.info.firstEdition}</div>
-                <div>{this.state.info.fundation}</div>
-                <div>{this.state.info.newspaper}</div>
-                <div>{this.state.info.contract}</div>
-                <h2>{this.state.info.centerHeading}</h2>
-                <div>{this.state.info.fundationCenter}</div>
-                <div>{this.state.info.history}</div>
-                <div>{this.state.info.art}</div>
-                <div>{this.state.info.base}</div>
+            <article id="home" className="row">
+                <div id="first-article" className="col-md-8  offset-md-2">
+                    <h1>{this.state.info.mainHeader}</h1>
+                    <h2>{this.state.info.teacher}</h2>
+                    <h2>{this.state.info.voice}</h2>
+                    <h2>{this.state.info.chairman}</h2>
+                    <div>{this.state.info.firstEdition}</div>
+                    {this.state.isTextVisible
+                        ? <div>
+                            <div>{this.state.info.fundation}</div>
+                            <div>{this.state.info.newspaper}</div>
+                            <div>{this.state.info.contract}</div></div>
+                        : null}
+                    <a href="#first-article" onClick={this.handleChange}>
+                        {this.state.isTextVisible ? "Прочети по-малко" : "Прочети повече"}</a>
+                </div>
+
             </article>
         )
     }
