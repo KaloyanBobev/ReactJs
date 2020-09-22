e58f8c44a522e7758b282e777acf899cba055a76
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-22 11:50:21 +0100

    add new handleChange button and bootstrap grid

--- literaturen-glas/src/components/Newspaper/Newspapers.js
+++ literaturen-glas/src/components/Newspaper/Newspapers.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './newspapers.scss'
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import newspapers from '../../data/newspaper.json';
 
@@ -6,8 +7,10 @@ class Newspapers extends React.Component {
     constructor() {
         super();
         this.state = {
-            news: []
+            isReverse: false,
+            news: [],
         }
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
@@ -16,22 +19,28 @@ class Newspapers extends React.Component {
         })
     }
 
+    handleChange() {
+        this.setState(prevState => ({
+            isReverse: !prevState.isReverse
+        }));
+        console.log(this.state.isReverse);
+    }
+
     render() {
         return (
-            <section>
-                {this.state.news.reverse().map((value) => {
+            <section className="newsparers">
+                <button onClick={this.handleChange}>{this.state.isReverse ? "Най-нови" : "Най-стари"}</button>
+                { this.state.news.reverse().map((value) => {
                     return (
-                        <article key={value.href}>
-                            <h2>{value.title}</h2>
-                            <h3>{value.date}</h3>
-                            <button>
+                        <article key={value.href} className="row align-items-center">
+                            <h2 className="col-md-3">{value.title}</h2>
+                            <h3 className="col-md-4">{value.date}</h3>
+                            <button className="align-self-center">
                                 <a href={require(`../../newspapers/${value.href}`)} rel="noopener noreferrer" target="_blank">
                                     &#xe043;   Прочети
                                 </a>
                             </button>
-
-
-                            <button>
+                            <button className="offset-md-1">
                                 <a href={require(`../../newspapers/${value.href}`)} download>
                                     &#xe118; Свали
                             </a>
