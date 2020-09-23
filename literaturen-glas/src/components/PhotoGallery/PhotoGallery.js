0ea3790b7b697141bc37867e7aa90723b74a02c5
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-23 20:50:05 +0100

    create the logic and JSX

--- literaturen-glas/src/components/PhotoGallery/PhotoGallery.js
+++ literaturen-glas/src/components/PhotoGallery/PhotoGallery.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import './PhotoGallery.scss';
+import imagesSorce from '../../data/images.json';
 
 class PhotoGallery extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            images: []
+        }
+    }
+    componentDidMount() {
+        this.setState({
+            images: imagesSorce
+        })
+    }
     render() {
         return (
-            <h1>PhotoGallery</h1>
+            <section id="gallery">
+                <h1>Всички снимки</h1>
+                <h3>Снимки на корици и вестници</h3>
+                <div className="row">
+                    {this.state.images.slice(0, 7).map((value) => {
+                        return (
+                            <div key={value.src} className="col=md-3 offset-md-1">
+                                <a href={require("../../images/books/" + value.src + ".jpg")}>
+                                    <img src={require("../../images/books/" + value.src + ".jpg")} title={value.src} alt="book covers" />
+                                </a>
+                            </div>
+                        )
+                    })}
+                </div>
+                <h3>Общи снимки</h3>
+                <div className="row">
+                    {this.state.images.slice(7, 28).map((value, index) => {
+                        return (
+                            <div key={index} className="col=md-3 offset-md-1">
+                                <a href={require("../../images/gallery/" + value.src + ".jpg")}>
+                                    <img src={require("../../images/gallery/" + value.src + ".jpg")} title={value.src} alt="important meetings" />
+                                </a>
+                            </div>
+                        )
+                    })}
+                    {this.state.images.slice(7, 28).map((value, index) => {
+                        return (
+                            <div key={index} className="col=md-3 offset-md-1">
+                                <a href={require("../../images/gallery/photos2020" + value.src + ".jpg")}>
+                                    <img src={require("../../images/gallery/photos2020" + value.src + ".jpg")} title={value.src} alt="important meetings" />
+                                </a>
+                            </div>
+                        )
+                    })}
+                </div>
+            </section>
         )
     }
 }
