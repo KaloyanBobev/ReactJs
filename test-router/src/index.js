ee0eeaf1d8e90632aa2b46ed19a9b5be10bc991d
kaloyanbobev <kaloyanbobev@gmail.com>
2020-09-25 13:22:13 +0100

    new test app

---
+++ test-router/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import Root from './Components/Root/Root';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Root />
+  </BrowserRouter>
+
+  ,
+  document.getElementById('root')
+);
+
+
