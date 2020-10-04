10ea6849b5990403a7aa28ef5495eac423c9f182
kaloyanbobev <kaloyanbobev@gmail.com>
2020-10-04 19:02:46 +0100

    remove some unuseful files

--- poet-voice/src/App.js
+++ poet-voice/src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Link, Switch } from 'react-router-dom';
+
+import Home from './components/home/Home';
+import Newspapers from './components/newspapers/Newspapers';
+import Books from './components/books/Books';
+import PhotoGallery from './components/photoGallery/PhotoGallery';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <nav>
+        <a href="home">Home</a> {}
+        <a href="newspepers">newspepers</a> {}
+        <a href="books">books</a> {}
+        <a href="photo-gallery">photo-gallery</a> {}
+        <a href="about">about</a> {}
+        <a href="contact">contact</a> {}
+        <Switch>
+          <Route component={Home} exact path="/" />
+          <Route component={Home} path="/home" />
+          <Route component={Newspapers} path="/newspepers" />
+          <Route component={Books} path="/books" />
+          <Route component={PhotoGallery} path="/photo-gallery" />
+          <Route component={About} path="/about" />
+          <Route component={Contact} path="/contact" />
+        </Switch>
+      </nav>
     </div>
   );
 }
