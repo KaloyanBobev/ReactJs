b675adaa373ee3abffdd277c94be1f7d5a9e05bb
kaloyanbobev <kaloyanbobev@gmail.com>
2020-10-04 19:03:05 +0100

    create the scafolding

---
+++ poet-voice/src/components/photoGallery/PhotoGallery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class PhotoGallery extends React.Component {
+
+    render() {
+        return (
+            <h1>PhotoGallery</h1>
+        )
+    }
+}
+
+export default PhotoGallery;
