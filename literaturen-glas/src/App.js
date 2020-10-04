9f98aac34a3a4c95b3bf6950bdaa1fda6f8405fe
kaloyanbobev <kaloyanbobev@gmail.com>
2020-10-04 19:23:02 +0100

    change text shadow and body width

--- literaturen-glas/src/App.js
+++ literaturen-glas/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 
 import Header from './components/Header/Header.js';
 import Main from './components/Main/Main.js';
-
 import Footer from './components/Footer/Footer';
 
 function App() {
@@ -14,14 +13,11 @@ function App() {
         <div className="col col-md-12">
           <BrowserRouter>
             <Header />
-
             <Main />
           </BrowserRouter>
           <Footer />
         </div>
       </div>
-
-
     </div>
   );
 }
