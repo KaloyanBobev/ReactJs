ce3d244fecfbb6a6659c2424de54db6a50905869
kaloyanbobev <kaloyanbobev@gmail.com>
2020-10-05 10:48:40 +0100

    corrects some errors

--- literaturen-glas/src/components/ContactUs/ContactUs.js
+++ literaturen-glas/src/components/ContactUs/ContactUs.js
@@ -9,9 +9,9 @@ class ContactUs extends React.Component {
                 <div>тел. 042 649110</div>
                 <div>GSM 0888 790135</div>
                 <div>
-                    <iframe class="googleMap" title="community center"
+                    <iframe className="googleMap" title="community center"
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d736.3596374086586!2d25.61543122922663!3d42.418399288754536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDLCsDI1JzA2LjIiTiAyNcKwMzYnNTcuNSJF!5e0!3m2!1sbg!2suk!4v1590675402039!5m2!1sbg!2suk"
-                        frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                        frameBorder="0" allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
                 </div>
             </section>
         )
