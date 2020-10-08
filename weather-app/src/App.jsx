66efe5674be883670dac5d97aab7cf3e61e99849
kaloyanbobev <kaloyanbobev@gmail.com>
2020-10-08 11:41:48 +0100

    rename App  to jsx

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './sass/app.scss'
+function App() {
+  return (
+    <div className="app-container">
+      <div className="main-container">Main Container</div>
+    </div>
+  );
+}
+
+export default App;
