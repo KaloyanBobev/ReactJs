303257aad2059be3fd2272696a8929e577307d1a
kaloyanbobev <kaloyanbobev@gmail.com>
2020-10-09 13:28:22 +0100

    create bottom component with style file

---
+++ weather-app/src/components/bottom/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class BottomSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+
+    }
+
+    render() {
+
+        return (
+            <div>Bottom</div>
+        )
+    }
+}
