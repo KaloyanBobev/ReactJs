e0ddcf62f2eceb288f44c26d9f40e6a50fcf1cb7
kaloyanbobev <kaloyanbobev@gmail.com>
2020-10-09 13:28:33 +0100

    create top component with style file

---
+++ weather-app/src/components/top/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class TopSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+
+    }
+
+    render() {
+
+        return (
+            <div className="cop-container">
+                <div className="title">Weather Up</div>
+                <div className=""></div>
+            </div>
+        )
+    }
+}
