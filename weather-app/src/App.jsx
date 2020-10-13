457f79c2984985a2b1637660614c65416380c3ac
kaloyanbobev <kaloyanbobev@gmail.com>
2020-10-13 11:11:19 +0100

    create eventEmitter and updateWeather

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -5,6 +5,8 @@ import './sass/app.scss'
 import TopSection from './components/top';
 import BottomSection from './components/bottom';
 
+
+
 const WEATHER_KEY = "2db66cfa44f64319a37102115201210";
 
 class App extends React.Component {
@@ -17,7 +19,7 @@ class App extends React.Component {
     }
   }
 
-  componentDidMount() {
+  updateWeather() {
     const { cityName, forecastDays } = this.state;
 
     const URL = `http://api.weatherapi.com/v1/forecast.json?key=${WEATHER_KEY} &q=${cityName} &days=${forecastDays}`;
@@ -31,7 +33,6 @@ class App extends React.Component {
           isDay: data.current.is_day,
           text: data.current.condition.text,
           iconURL: data.current.condition.icon,
-
         })
       })
       .catch((err) => {
@@ -42,6 +43,18 @@ class App extends React.Component {
 
   }
 
+  componentDidMount() {
+    const { eventEmitter } = this.props;
+
+    this.updateWeather();
+
+    eventEmitter.on("updateWeather", (data) => {
+      this.setState({ cityName: data }, () => { this.updateWeather(); })
+
+      console.log("LocationName", data);
+    })
+  }
+
   render() {
     const { isLoading, cityName, temp_c, isDay, text, iconURL } = this.state;
 
@@ -49,7 +62,14 @@ class App extends React.Component {
       <div className="app-container" >
         <div className="main-container">
           {!isLoading && <h1>Loading Weather...</h1>}
-          {isLoading && <div className="top-section"><TopSection location={cityName} temp_c={temp_c} isDay={isDay} text={text} iconURL={iconURL} /></div>}
+          {isLoading && <div className="top-section"><TopSection
+            location={cityName}
+            temp_c={temp_c}
+            isDay={isDay}
+            text={text}
+            iconURL={iconURL}
+            eventEmitter={this.props.eventEmitter}
+          /></div>}
           <div className="bottom-section"><BottomSection /></div>
         </div>
       </div>
