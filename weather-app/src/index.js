525db4293fd24a57df321555f4b1bc020cfa98cf
kaloyanbobev <kaloyanbobev@gmail.com>
2020-10-13 11:12:47 +0100

    add store component

--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App.jsx';
-
+import Store from './Store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Store>
+      <App />
+    </Store>
   </React.StrictMode>,
   document.getElementById('root')
 );
