21c08dfef64aa43959f1c9aba1d909aeea725011
kaloyanbobev <kaloyanbobev@gmail.com>
2020-10-14 16:28:19 +0100

    create a forecastday

--- weather-app/src/components/bottom/forecast.jsx
+++ weather-app/src/components/bottom/forecast.jsx
@@ -4,10 +4,15 @@ import React from 'react';
 export default class Forecastday extends React.Component {
 
     render() {
+        const { day } = this.props;
+        if (!day) return null;
+
         return (
             <div className="forecast-container">
-                <div className="image"><img src="//cdn.weatherapi.com/weather/64x64/day/176.png" alt="forecast image"></img></div>
-                <div className="text">Sunny</div>
+
+                <div className="image"><img src={day.condition.icon} alt="forecast"></img></div>
+                <div className="text">{day.avgtemp_c}&#176;</div>
+                <div className="muted-text">{day.condition.text}</div>
             </div>
         )
     }
