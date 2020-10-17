2880619edd042c2eaea2d4138f2b16abc34f51b3
kaloyanbobev <kaloyanbobev@gmail.com>
2020-10-17 20:16:54 +0100

    update

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -12,9 +12,9 @@ function App() {
   const [weather, setWeather] = useState({});
 
   const search = evt => {
-    if (evt.key === "enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key} `)
-        .then(res => res.json)
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
         .then(result => {
           setWeather(result);
           setQuery('');
@@ -36,25 +36,34 @@ function App() {
   }
 
   return (
-    <div className="app warm">
+    <div className={(typeof weather.main != "undefined")
+      ? (weather.main.temp > 16
+        ? 'app warm'
+        : 'app')
+      : 'app'}>
       <main>
         <div className="search-box">
-          <input type="text"
+          <input
+            type="text"
             className="search-bar"
-            placeholder="Search.."
+            placeholder="Search..."
             onChange={e => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
           />
         </div>
-        <div className="location-box">
-          <div className="location">New York City</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">15&#176;C</div>
-          <div className="weather">Sunny</div>
-        </div>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className="location-box">
+              <div className="location">{weather.name},{weather.sys.country}</div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="weather">{weather.weather[0].main}</div>
+              <div className="temp">{Math.round(weather.main.temp)}&#176;C</div>
+            </div>
+          </div>
+        ) : ('')}
       </main>
 
     </div>
