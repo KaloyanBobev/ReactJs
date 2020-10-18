f5e492a5dbf557e3057425c706136278043eb6c4
kaloyanbobev <kaloyanbobev@gmail.com>
2020-10-18 20:57:26 +0100

    create side-nav component

---
+++ literaturen-glas/src/components/SideNav/SideNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './SideNav.scss';
+
+export default class SideNav extends React.Component {
+
+    render() {
+        return (
+            <div className="side-nav">Side Nav</div>
+        )
+    }
+}
