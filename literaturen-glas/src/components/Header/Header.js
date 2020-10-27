5ecdebc479721fa07c6beb8a5d54cd9a499e8db2
kaloyanbobev <kaloyanbobev@gmail.com>
2020-10-27 18:55:17 +0000

    add the language buttons and their functionallaty

--- literaturen-glas/src/components/Header/Header.js
+++ literaturen-glas/src/components/Header/Header.js
@@ -3,43 +3,51 @@ import './Header.scss';
 
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import img from "../../images/raphael-schaller-navbar-2.jpg";
+import { useTranslation } from 'react-i18next';
 
-class Header extends React.Component {
+function Header() {
 
-    render() {
-        const name = "Литературен глас";
-        const home = "Начало";
-        const publishedEditions = "Печатни издания";
-        const newspapers = "Вестници";
-        const books = "Книги и литературни произведения";
-        const photoGallery = "Фото галерия";
-        const forUs = "За нас";
-        const contacts = "Контакти";
+    const { t, i18n } = useTranslation();
 
-        return (
-            <div>
-                <header>
-                    <Navbar expand="lg" sticky="top">
-                        <Navbar.Brand href="/" className="logo">{name}</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="mr-auto ">
-                                <Nav.Link href="/home" >{home}</Nav.Link>
-                                <NavDropdown title={publishedEditions} id="basic-nav-dropdown" className="drop-down-menu">
-                                    <NavDropdown.Item href="/newspapers" >{newspapers}</NavDropdown.Item>
-                                    <NavDropdown.Item href="/books" >{books}</NavDropdown.Item>
-                                </NavDropdown>
-                                <Nav.Link href="/photo-gallery">{photoGallery}</Nav.Link>
-                                <Nav.Link href="/about" >{forUs}</Nav.Link>
-                                <Nav.Link href="/contact" >{contacts}</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Navbar>
-                </header>
-                <img className="img-fluid" src={img} alt="navbar background" />
-            </div >
-        )
+    function handleClick(lang) {
+        i18n.changeLanguage(lang);
     }
+
+
+
+    return (
+        <div>
+            <header>
+                <Navbar expand="lg" sticky="top">
+                    <Navbar.Brand href="/" className="logo">{t('name')}</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto ">
+                            <Nav.Link href="/home" >{t('home')}</Nav.Link>
+                            <NavDropdown title={t('publishedEditions')} id="basic-nav-dropdown" className="drop-down-menu">
+                                <NavDropdown.Item href="/newspapers" >{t('newspapers')}</NavDropdown.Item>
+                                <NavDropdown.Item href="/books" >{t('books')}</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="/photo-gallery">{t('photoGallery')}</Nav.Link>
+                            <Nav.Link href="/about" >{t('forUs')}</Nav.Link>
+                            <Nav.Link href="/contact" >{t('contacts')}</Nav.Link>
+                        </Nav>
+                        <div className="language-btn">
+                            <button onClick={() => handleClick('bg')}>
+                                Bul
+                            </button>
+                            <button onClick={() => handleClick('en')}>
+                                Eng
+                            </button>
+                        </div>
+                    </Navbar.Collapse>
+                </Navbar>
+
+            </header>
+            <img className="img-fluid" src={img} alt="navbar background" />
+        </div >
+    )
 }
+
 
 export default Header;
