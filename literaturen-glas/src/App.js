af9b9d55249e70e7ae23278aa3054a5ce9b723c3
kaloyanbobev <kaloyanbobev@gmail.com>
2020-10-28 10:10:57 +0000

    add suspense

--- literaturen-glas/src/App.js
+++ literaturen-glas/src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter } from "react-router-dom";
 
 import Header from './components/Header/Header.js';
 import Main from './components/Main/Main.js';
 import Footer from './components/Footer/Footer';
+import './i18next';
 
 function App() {
 
   return (
     <div>
       <BrowserRouter>
-        <Header />
+        <Suspense fallback={<div>Loading</div>}>
+          <Header />
+        </Suspense>
         <Main />
       </BrowserRouter>
       <Footer />
