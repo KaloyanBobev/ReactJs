e922e1062a135229093e3b51c320869a976432b8
kaloyanbobev <kaloyanbobev@gmail.com>
2020-10-29 21:11:03 +0000

    add suspense

--- literaturen-glas/src/components/Main/Main.js
+++ literaturen-glas/src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import AboutUs from '../AboutUs/AboutUs';
@@ -14,16 +14,18 @@ class Main extends React.Component {
     render() {
         return (
             <main>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/home" component={Home} />
-                    <Route path="/newspapers" component={Newspapers} />
-                    <Route path="/books" component={Books} />
-                    <Route path="/photo-gallery" component={PhotoGallery} />
-                    <Route path="/about" component={AboutUs} />
-                    <Route path="/contact" component={ContactUs} />
-                    <Route path="**" component={NotFound} />
-                </Switch>
+                <Suspense fallback={<div>Loading</div>}>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/home" component={Home} />
+                        <Route path="/newspapers" component={Newspapers} />
+                        <Route path="/books" component={Books} />
+                        <Route path="/photo-gallery" component={PhotoGallery} />
+                        <Route path="/about" component={AboutUs} />
+                        <Route path="/contact" component={ContactUs} />
+                        <Route path="**" component={NotFound} />
+                    </Switch>
+                </Suspense>
             </main>
         )
     }
