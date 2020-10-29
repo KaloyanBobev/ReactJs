f3f64f1b01f4ab00ba4c6f48952475f7a58cb8e1
kaloyanbobev <kaloyanbobev@gmail.com>
2020-10-29 21:06:30 +0000

    update with translation

--- literaturen-glas/src/components/Books/Book.js
+++ literaturen-glas/src/components/Books/Book.js
@@ -1,32 +1,35 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-class Book extends React.Component {
+function Book({ poems, tracked }) {
+    const { t } = useTranslation();
 
 
-    render() {
-        return (
-            <div>
-                <article className="row">
-                    <h3 className="col-md-3 ">Име на автора</h3>
-                    <h3 className="col-md-3">Заглавие</h3>
-                    <h3 className="col-md-3">Жанр</h3>
-                    <h3 className="col-md-3">Дата на издаване</h3>
-                </article>
-                {
-                    this.props.poems.filter(name => name.author.toLocaleLowerCase().includes(this.props.tracked.toLocaleLowerCase())).map((book) => {
-                        return (
-                            <article key={book.id} className="row">
-                                <div className="col-md-3 ">{book.author}</div>
-                                <div className="col-md-3">{book.name}</div>
-                                <div className="col-md-3">{book.species}</div>
-                                <div className="col-md-3">{book.date}</div>
-                            </article>
-                        )
-                    })
-                }
-            </div>
-        )
-    }
+
+    return (
+        <div>
+            <article className="row">
+                <h3 className="col-md-3">{t('booksTrans.author')}</h3>
+                <h3 className="col-md-3">{t('booksTrans.name')}</h3>
+                <h3 className="col-md-3">{t('booksTrans.style')}</h3>
+                <h3 className="col-md-3">{t('booksTrans.issueDate')}</h3>
+            </article>
+
+            {
+                poems.filter(name => name.author.toLocaleLowerCase().includes(tracked.toLocaleLowerCase())).map((book) => {
+                    return (
+                        <article key={book.id} className="row">
+                            <div className="col-md-3 ">{book.author}</div>
+                            <div className="col-md-3">{book.name}</div>
+                            <div className="col-md-3">{book.species}</div>
+                            <div className="col-md-3">{book.date}</div>
+                        </article>
+                    )
+                })
+            }
+        </div>
+    )
 }
+
 
 export default Book;
--- literaturen-glas/src/components/Books/Books.js
+++ literaturen-glas/src/components/Books/Books.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import books from '../../data/books.json';
 import './Books.scss';
 import Book from './Book';
+import { withTranslation } from 'react-i18next';
+
+
+class LegacyComponentClass extends React.Component {
+
+    render() {
+        const { t } = this.props;
+
+        return (
+            <>
+                <h1 className="center">{t('booksTrans.header')}</h1>
+                <label>
+                    {t('booksTrans.sort')}
+                </label>
+
+            </>
+        )
+    }
+}
+
+const MyComponent = withTranslation()(LegacyComponentClass)
 
 class Books extends React.Component {
     constructor() {
@@ -28,18 +49,17 @@ class Books extends React.Component {
 
     render() {
         return (
-            <section id="books">
-                <h1 className="center">Списък произведения</h1>
-                <label>Сортирай по име на автор:
-
-                </label>
-                <input
-                    placeholder=""
-                    value={this.state.tracked}
-                    onChange={this.filterHadler}
-                />
-                <Book poems={this.state.poems} tracked={this.state.tracked} />
-            </section>
+            <Suspense fallback="loading">
+                <section id="books">
+                    <MyComponent />
+                    <input
+                        placeholder=""
+                        value={this.state.tracked}
+                        onChange={this.filterHadler}
+                    />
+                    <Book poems={this.state.poems} tracked={this.state.tracked} />
+                </section>
+            </Suspense>
         )
     }
 }
