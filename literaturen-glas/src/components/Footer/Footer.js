d9f45f82934fc2d0778100ba6033e5f5f5f5bd64
kaloyanbobev <kaloyanbobev@gmail.com>
2020-10-30 15:52:58 +0000

    add translation

--- literaturen-glas/src/components/Footer/Footer.js
+++ literaturen-glas/src/components/Footer/Footer.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import './Footer.scss';
+import { withTranslation } from 'react-i18next';
+
+class LegacyComponentClass extends React.Component {
+
+    render() {
+        const { t } = this.props;
+
+        return (
+            <div id="footer">
+                {t('footerPage.1')} {this.props.currentYear}
+            </div>
+        )
+    }
+}
+
+const MyComponent = withTranslation()(LegacyComponentClass);
+
 class Footer extends React.Component {
     constructor() {
         super();
@@ -17,9 +34,12 @@ class Footer extends React.Component {
 
     render() {
         return (
-            <div id="footer">
-                Всички права запазени. CopyRight &#9400; година: 2015-{this.state.date}
-            </div>
+
+            <MyComponent currentYear={this.state.date} />
+            // <div id="footer">
+
+            //     Всички права запазени. CopyRight &#9400; година: 2015-{this.state.date}
+            // </div>
         )
     }
 }
