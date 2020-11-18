6f61cea63250814a51d443c67c35da905ed88cc8
kaloyanbobev <kaloyanbobev@gmail.com>
2020-11-18 11:51:46 +0000

    create navbar

--- resort-reservation/src/App.js
+++ resort-reservation/src/App.js
@@ -1,9 +1,32 @@
-
+import React from 'react';
 import './App.css';
+import { Link, Switch, Route } from 'react-router-dom';
+
+
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom'
+
 
 function App() {
   return (
-    <>Hello</>
+    <>
+
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/rooms"> Rooms </Link>
+        <Link to="/single-room"> SingleRoom </Link>
+        <Link to="**">Error</Link>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/rooms" component={Rooms}> </Route>
+        <Route path="/single-room" component={SingleRoom}> </Route>
+
+      </Switch>
+
+
+    </>
   );
 }
 
