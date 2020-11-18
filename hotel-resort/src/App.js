a2eddc539721f2bf6788c651f1b9a2e354684f5b
kaloyanbobev <kaloyanbobev@gmail.com>
2020-11-18 18:52:25 +0000

    add routes and components

--- hotel-resort/src/App.js
+++ hotel-resort/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
-//import './App.css';
+import './App.scss';
 
 
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
-
+import Error from './pages/Error';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <nav>
-        <Link to="/">Home </Link>
-        <Link to="/rooms"> Rooms </Link>
-        <Link to="/single-room"> Single room </Link>
-      </nav>
-      <Route exact path="/" component={Home} />
-      <Route path="/rooms" component={Rooms} />
-      <Route path="/single-room" component={SingleRoom} />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
