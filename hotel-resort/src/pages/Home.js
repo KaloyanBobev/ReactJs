a52fd010bbad8cb37e814b7ae2f1a754356f4ebf
kaloyanbobev <kaloyanbobev@gmail.com>
2020-11-20 13:23:30 +0000

    add Hero, Banner & Services components

--- hotel-resort/src/pages/Home.js
+++ hotel-resort/src/pages/Home.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import Services from '../components/Services';
 
 export default function Home() {
     return (
-        <div>
-            Home page
-        </div>
+        <>
+            <Hero hero={'defaultHero'}>
+                <Banner
+                    title="luxurious rooms"
+                    subtitle="deluxe room starting at £299" >
+                    <Link to='/rooms' className="btn-primary">our rooms</Link>
+                </Banner>
+            </Hero>
+            <Services />
+        </>
     )
 }
