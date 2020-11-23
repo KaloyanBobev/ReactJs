ac449596489ba72436798576fbfa349509a8e468
kaloyanbobev <kaloyanbobev@gmail.com>
2020-11-23 20:40:30 +0000

    add RoomProvider

--- hotel-resort/src/index.js
+++ hotel-resort/src/index.js
@@ -4,13 +4,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { RoomProvider } from './Context';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+  <RoomProvider>
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  </RoomProvider>,
   document.getElementById('root')
 );
 
