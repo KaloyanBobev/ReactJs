95fcdae9ed6234a633709fb1f639a150be3112b7
kaloyanbobev <kaloyanbobev@gmail.com>
2020-11-25 13:01:09 +0000

    update

--- hotel-resort/src/components/FeaturedRooms.js
+++ hotel-resort/src/components/FeaturedRooms.js
@@ -3,11 +3,8 @@ import React, { Component } from 'react'
 import { RoomContext } from '../Context';
 
 export default class FeaturedRooms extends Component {
-
     static contextType = RoomContext;
-
     render() {
-
         return (
             <div>
                 featured room
