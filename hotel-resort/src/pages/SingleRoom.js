c9939ba1f24a1d881c8861b8d609bd7758edba09
kaloyanbobev <kaloyanbobev@gmail.com>
2020-11-26 15:17:17 +0000

    add state,contextType

--- hotel-resort/src/pages/SingleRoom.js
+++ hotel-resort/src/pages/SingleRoom.js
@@ -6,13 +6,24 @@ import { Link } from 'react-router-dom';
 import { RoomContext } from '../Context';
 
 
-export default class SingleRoom extends Comment {
+export default class SingleRoom extends React.Component {
     constructor(props) {
         super(props);
+        // console.log(this.props);
+        this.state = {
+            slug: this.props.match.params.slug,
+            defaultBcg
+        }
+    }
+    static contextType = RoomContext;
+
+    componentDidMount() {
 
     }
 
     render() {
+        const { getRoom } = this.context;
+        const name = getRoom(this.state.slug);
         return (
             <div>
                 Single Room  Page
