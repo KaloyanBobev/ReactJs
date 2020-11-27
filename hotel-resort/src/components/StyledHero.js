cbf76e6d8c06ff9c48cd2d2cd5da0943d4c93ef5
kaloyanbobev <kaloyanbobev@gmail.com>
2020-11-27 19:03:35 +0000

    create styled hero

---
+++ hotel-resort/src/components/StyledHero.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+const red = "#f15025";
+const SimpleButton = styled.button`
+ color:${red};
+ background:green;
+font-size:3rem;
+`;
+
+
+export default SimpleButton;
