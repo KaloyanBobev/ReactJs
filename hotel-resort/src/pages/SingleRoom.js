2f36574ef7c080d42ef88de3af4e9bf706efeb98
kaloyanbobev <kaloyanbobev@gmail.com>
2020-11-27 14:06:09 +0000

    create Hero

--- hotel-resort/src/pages/SingleRoom.js
+++ hotel-resort/src/pages/SingleRoom.js
@@ -17,17 +17,40 @@ export default class SingleRoom extends React.Component {
     }
     static contextType = RoomContext;
 
-    componentDidMount() {
+    // componentDidMount() {
 
-    }
+    // }
 
     render() {
         const { getRoom } = this.context;
-        const name = getRoom(this.state.slug);
-        return (
-            <div>
-                Single Room  Page
-            </div>
-        )
+        const room = getRoom(this.state.slug);
+        if (!room) {
+            return (<div className="error">
+                <h3>No such room could be found</h3>
+                <Link to='/rooms' className="btn-primary">
+                    back to rooms
+                </Link>
+            </div>);
+        }
+        const {
+            name,
+            description,
+            capacity,
+            size,
+            price,
+            extras,
+            breakfast,
+            pets,
+            images } = room;
+
+        return <Hero hero='roomsHero'>
+            <Banner title={`${name} room`}>
+                <Link
+                    to='/rooms'
+                    className="btn-primary">
+                    back to rooms
+                    </Link>
+            </Banner>
+        </Hero >
     }
 }
