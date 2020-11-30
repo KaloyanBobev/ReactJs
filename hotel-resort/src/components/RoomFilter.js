1f4d503dbb7d607d0737bd546991c7c77289df4c
kaloyanbobev <kaloyanbobev@gmail.com>
2020-11-30 17:14:35 +0000

    create the section

--- hotel-resort/src/components/RoomFilter.js
+++ hotel-resort/src/components/RoomFilter.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import { useContext } from 'react';
+import { RoomContext } from '../Context';
+import Title from '../components/Title';
 
 export default function RoomFilter() {
+    const context = useContext(RoomContext);
+    const {
+        handleChange,
+        type,
+        capacity,
+        price,
+        minPrice,
+        maxPrice,
+        minSize,
+        maxSize,
+        breakfast,
+        pets
+    } = context;
     return (
-        <div>
-            hello from RoomFilter
-        </div>
+        <section className="filter-container">
+            <Title title="serarch rooms" />
+            <form className="filter-form">
+                {/*select type */}
+                <div className="form-group">
+                    <label htmlFor="type">room type </label>
+                    <select
+                        name="type"
+                        id="type"
+                        value={type} c
+                        lassName="form-control"
+                        onChange={handleChange}
+                    />
+                </div>
+
+                {/*end select type */}
+            </form>
+        </section>
     )
 }
