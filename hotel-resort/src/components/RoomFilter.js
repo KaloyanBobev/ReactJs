2712bf5cdb7371843b291cbac6ce4b851eb14f61
kaloyanbobev <kaloyanbobev@gmail.com>
2020-12-01 16:52:46 +0000

    create select menu

--- hotel-resort/src/components/RoomFilter.js
+++ hotel-resort/src/components/RoomFilter.js
@@ -3,7 +3,11 @@ import { useContext } from 'react';
 import { RoomContext } from '../Context';
 import Title from '../components/Title';
 
-export default function RoomFilter() {
+const getUnique = (items, value) => {
+    return [...new Set(items.map(item => item[value]))]
+}
+
+export default function RoomFilter({ rooms }) {
     const context = useContext(RoomContext);
     const {
         handleChange,
@@ -17,6 +21,15 @@ export default function RoomFilter() {
         breakfast,
         pets
     } = context;
+
+    let types = getUnique(rooms, 'type');
+
+    types = ['all', ...types];
+
+    types = types.map((item, index) => {
+        return <option value={item} key={index}>{item} </option>
+    })
+
     return (
         <section className="filter-container">
             <Title title="serarch rooms" />
@@ -30,7 +43,9 @@ export default function RoomFilter() {
                         value={type} c
                         lassName="form-control"
                         onChange={handleChange}
-                    />
+                    >
+                        {types}
+                    </select>
                 </div>
 
                 {/*end select type */}
